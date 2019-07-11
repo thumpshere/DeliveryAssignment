1d3c54c33399bfa05aeaebc7f87198919b3c2bd7
Arpit <arpit.tripathi@nagarro.com>
2019-07-11 17:32:12 +0530

    added project env

---
+++ Delivery AssignmentTests/ListViewControllerTest.swift
@@ -0,0 +1,55 @@
+//
+//  ListViewControllerTest.swift
+//  Delivery AssignmentTests
+//
+//  Created by Arpit Tripathi on 10/07/19.
+//  Copyright © 2019 Arpit Tripathi. All rights reserved.
+//
+
+import XCTest
+@testable import Delivery_Assignment
+class ListViewControllerTest: XCTestCase {
+  var deliveryListVC: ListViewController = ListViewController()
+  
+  override func setUp() {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+    let navController = AppDelegate.delegate().window?.rootViewController as! UINavigationController
+    deliveryListVC = navController.viewControllers[0] as! ListViewController
+    deliveryListVC.viewDidLoad()
+  }
+
+  func testRequiredElementShouldNotNil() {
+    XCTAssertNotNil(deliveryListVC.title)
+    XCTAssertNotNil(deliveryListVC.listTable)
+    XCTAssertNotNil(deliveryListVC.refreshControl)
+    XCTAssertNotNil(deliveryListVC.noDataLabel)
+  }
+  
+  func testTableViewDelegateDataSource() {
+    XCTAssertTrue(deliveryListVC.conforms(to: UITableViewDelegate.self))
+    XCTAssertTrue(deliveryListVC.conforms(to: UITableViewDataSource.self))
+  }
+  
+  func testCellConfiguration() {
+    let tableView = deliveryListVC.listTable
+    let cell = deliveryListVC.tableView(tableView, cellForRowAt: IndexPath(row: 0, section: 0))
+    XCTAssertNotNil(cell)
+  }
+  
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
---
+++ Podfile
@@ -0,0 +1,29 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Delivery Assignment' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+pod 'Alamofire', '~> 5.0.0-beta.5'
+pod 'SDWebImage', '~> 5.0'
+pod 'Cache'
+pod 'SwiftLint'
+pod 'PKHUD', '~> 5.0'
+pod 'Firebase/Core'
+pod 'Fabric', '~> 1.10.2'
+pod 'Crashlytics', '~> 3.13.3'
+  # Pods for Delivery Assignment
+
+  target 'Delivery AssignmentTests' do
+    inherit! :search_paths
+    pod 'OHHTTPStubs/Swift', '~> 7.0.0'
+
+    # Pods for testing
+  end
+
+  target 'Delivery AssignmentUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
